bc5a86bfb3b5dabaec5e276b16ed6a2741eee62b
Alwin Arrasyid <alwin.ridd@gmail.com>
2017-10-21 00:28:37 +0700

    Using adafruit BME280 instead of BMP085

--- oled_bmp/src/OledBmp.cpp
+++ oled_bmp/src/OledBmp.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
-#include <Adafruit_BMP085.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
+#include <Adafruit_BME280.h>
 
 #define OLED_SDA 21
 #define OLED_SCL 22
 
 Adafruit_SH1106 display(OLED_SDA, OLED_SCL);
-Adafruit_BMP085 bmp;
+Adafruit_BME280 bme;
 
 void readAndDisplayBMPData();
 void drawText(const char* text, int x, int y);
 
 void setup() {
     Serial.begin(9600);
-    if (!bmp.begin()) {
-	    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    if (!bme.begin()) {
+	    Serial.println("Could not find a valid BME280 sensor, check wiring!");
 	    while (1) {}
     }
 
-    display.begin(SH1106_SWITCHCAPVCC, 0x3C); 
+    display.begin(SH1106_SWITCHCAPVCC, 0x3C);
     display.display();
     delay(2000);
     display.clearDisplay();
@@ -34,9 +34,9 @@ void readAndDisplayBMPData() {
     display.clearDisplay();
 
     String msg = "";
-    float temperatureC = bmp.readTemperature();
-    float pressure = bmp.readPressure();
-    float altitude = bmp.readAltitude();
+    float temperatureC = bme.readTemperature();
+    float pressure = bme.readPressure();
+    float humidity = bme.readHumidity();
 
     msg = "Temperature = ";
     msg += temperatureC;
@@ -44,7 +44,6 @@ void readAndDisplayBMPData() {
 
     Serial.println(msg);
     drawText(msg.c_str(), 0, 0);
-    
 
     msg = "Pressure = ";
     msg += (pressure / 1000);
@@ -52,9 +51,9 @@ void readAndDisplayBMPData() {
     Serial.println(msg);
     drawText(msg.c_str(), 0, 24);
 
-    msg = "Altitude = ";
-    msg += altitude;
-    msg += " m";
+    msg = "Humidity = ";
+    msg += humidity;
+    msg += " ";
     Serial.println(msg);
     drawText(msg.c_str(), 0, 48);
 }
